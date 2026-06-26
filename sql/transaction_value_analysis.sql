87f777d1bfcacf98c9755b1f11da0b087fac8037
Swatika <83584049+swatika1307@users.noreply.github.com>
2026-06-26 19:55:05 +0530

    04 - Transaction value analysis

--- sql/transaction_value_analysis.sql
+++ sql/transaction_value_analysis.sql
@@ -6,3 +6,13 @@ SELECT
 	ROUND(SUM(amt), 2) AS total_fraud_value
 FROM transactions_master
 WHERE is_fraud = 1;
+
+SELECT
+	ROUND(
+		(
+			SUM(CASE WHEN is_fraud = 1 THEN amt ELSE 0 END)
+            * 100.0
+		) / SUM(amt),
+	4)
+    AS fraud_value_percentage
+FROM transactions_master;
